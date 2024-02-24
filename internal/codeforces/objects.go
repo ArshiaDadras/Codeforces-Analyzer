a59286740f2fb059c7a39ab01d91ff26b18111e4
ArshiaDadras <arshiadadras1234@gmail.com>
2024-02-25 01:41:02 +0330

    codeforces api addresses added

---
+++ internal/codeforces/objects.go
@@ -0,0 +1,171 @@
+package codeforces
+
+type User struct {
+	Handle                  string `json:"handle"`
+	Email                   string `json:"email"`
+	VkId                    string `json:"vkId"`
+	OpenID                  string `json:"openId"`
+	FirstName               string `json:"firstName"`
+	LastName                string `json:"lastName"`
+	Country                 string `json:"country"`
+	City                    string `json:"city"`
+	Organization            string `json:"organization"`
+	Contributions           int    `json:"contribution"`
+	Rank                    string `json:"rank"`
+	Rating                  int    `json:"rating"`
+	MaxRank                 string `json:"maxRank"`
+	MaxRating               int    `json:"maxRating"`
+	LastOnlineTimeSeconds   int    `json:"lastOnlineTimeSeconds"`
+	RegistrationTimeSeconds int    `json:"registrationTimeSeconds"`
+	FriendOfCount           int    `json:"friendOfCount"`
+	Avatar                  string `json:"avatar"`
+	TitlePhoto              string `json:"titlePhoto"`
+}
+
+type BlogEntry struct {
+	ID                      int      `json:"id"`
+	OriginalLocale          string   `json:"originalLocale"`
+	CreationTimeSeconds     int      `json:"creationTimeSeconds"`
+	AuthorHandle            string   `json:"authorHandle"`
+	Title                   string   `json:"title"`
+	Content                 string   `json:"content"`
+	Locale                  string   `json:"locale"`
+	ModificationTimeSeconds int      `json:"modificationTimeSeconds"`
+	AllowViewHistory        bool     `json:"allowViewHistory"`
+	Tags                    []string `json:"tags"`
+	Rating                  int      `json:"rating"`
+}
+
+type Comment struct {
+	ID                  int    `json:"id"`
+	CreationTimeSeconds int    `json:"creationTimeSeconds"`
+	CommentatorHandle   string `json:"commentatorHandle"`
+	Locale              string `json:"locale"`
+	Text                string `json:"text"`
+	ParentCommentId     int    `json:"parentCommentId"`
+	Rating              int    `json:"rating"`
+}
+
+type RecentAction struct {
+	TimeSeconds int       `json:"timeSeconds"`
+	BlogEntry   BlogEntry `json:"blogEntry"`
+	Comment     Comment   `json:"comment"`
+}
+
+type RatingChange struct {
+	ContestID               int    `json:"contestId"`
+	ContestName             string `json:"contestName"`
+	Handle                  string `json:"handle"`
+	Rank                    int    `json:"rank"`
+	RatingUpdateTimeSeconds int    `json:"ratingUpdateTimeSeconds"`
+	OldRating               int    `json:"oldRating"`
+	NewRating               int    `json:"newRating"`
+}
+
+type Contest struct {
+	ID                  int    `json:"id"`
+	Name                string `json:"name"`
+	Type                string `json:"type"`
+	Phase               string `json:"phase"`
+	Frozen              bool   `json:"frozen"`
+	DurationSeconds     int    `json:"durationSeconds"`
+	StartTimeSeconds    int    `json:"startTimeSeconds"`
+	RelativeTimeSeconds int    `json:"relativeTimeSeconds"`
+	PreparedBy          string `json:"preparedBy"`
+	WebsiteURL          string `json:"websiteUrl"`
+	Description         string `json:"description"`
+	Kind                string `json:"kind"`
+	IcpcRegion          string `json:"icpcRegion"`
+	Country             string `json:"country"`
+	City                string `json:"city"`
+	Season              string `json:"season"`
+}
+
+type Party struct {
+	ContestID        int    `json:"contestId"`
+	Members          []User `json:"members"`
+	ParticipantType  string `json:"participantType"`
+	TeamID           int    `json:"teamId"`
+	TeamName         string `json:"teamName"`
+	Ghost            bool   `json:"ghost"`
+	Room             int    `json:"room"`
+	StartTimeSeconds int    `json:"startTimeSeconds"`
+}
+
+type Member struct {
+	Handle string `json:"handle"`
+	Name   string `json:"name"`
+}
+
+type Problem struct {
+	ContestID      int      `json:"contestId"`
+	ProblemsetName string   `json:"problemsetName"`
+	Index          string   `json:"index"`
+	Name           string   `json:"name"`
+	Type           string   `json:"type"`
+	Points         float64  `json:"points"`
+	Rating         int      `json:"rating"`
+	Tags           []string `json:"tags"`
+}
+
+type ProblemStatistics struct {
+	ContestID   int    `json:"contestId"`
+	Index       string `json:"index"`
+	SolvedCount int    `json:"solvedCount"`
+}
+
+type ProblemResult struct {
+	Points                    float64 `json:"points"`
+	Penalty                   int     `json:"penalty"`
+	RejectedAttemptCount      int     `json:"rejectedAttemptCount"`
+	Type                      string  `json:"type"`
+	BestSubmissionTimeSeconds int     `json:"bestSubmissionTimeSeconds"`
+}
+
+type Submission struct {
+	ID                  int     `json:"id"`
+	ContestID           int     `json:"contestId"`
+	CreationTimeSeconds int     `json:"creationTimeSeconds"`
+	RelativeTimeSeconds int     `json:"relativeTimeSeconds"`
+	Problem             Problem `json:"problem"`
+	Author              User    `json:"author"`
+	ProgrammingLanguage string  `json:"programmingLanguage"`
+	Verdict             string  `json:"verdict"`
+	Testset             string  `json:"testset"`
+	PassedTestCount     int     `json:"passedTestCount"`
+	TimeConsumedMillis  int     `json:"timeConsumedMillis"`
+	MemoryConsumedBytes int     `json:"memoryConsumedBytes"`
+	Points              float64 `json:"points"`
+}
+
+type Hack struct {
+	ID                  int     `json:"id"`
+	CreationTimeSeconds int     `json:"creationTimeSeconds"`
+	Hacker              Party   `json:"hacker"`
+	Defender            Party   `json:"defender"`
+	Verdict             string  `json:"verdict"`
+	Problem             Problem `json:"problem"`
+	Test                string  `json:"test"`
+	JudgeProtocol       struct {
+		Manual   string `json:"manual"`
+		Protocol string `json:"protocol"`
+		Verdict  string `json:"verdict"`
+	} `json:"judgeProtocol"`
+}
+
+type RanklistRow struct {
+	Party                     Party           `json:"party"`
+	Rank                      int             `json:"rank"`
+	Points                    float64         `json:"points"`
+	Penalty                   int             `json:"penalty"`
+	SuccessfulHackCount       int             `json:"successfulHackCount"`
+	UnsuccessfulHackCount     int             `json:"unsuccessfulHackCount"`
+	ProblemResults            []ProblemResult `json:"problemResults"`
+	LastSubmissionTimeSeconds int             `json:"lastSubmissionTimeSeconds"`
+}
+
+type Standings struct {
+	Contest  Contest       `json:"contest"`
+	Problems []Problem     `json:"problems"`
+	Rows     []RanklistRow `json:"rows"`
+}
---
+++ internal/codeforces/requests.go
@@ -0,0 +1,357 @@
+package codeforces
+
+import (
+	"crypto/sha512"
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"time"
+)
+
+func SortedParams(url string) string {
+	params := strings.Split(strings.Split(url, "?")[1], "&")
+	sort.Strings(params)
+
+	return strings.Split(url, "?")[0] + "?" + strings.Join(params, "&")
+}
+
+func GenerateAPISig(url, secret string) string {
+	SHA512 := sha512.New()
+	rnd := rand.Intn(1000000-100000) + 100000
+	SHA512.Write([]byte(fmt.Sprintf("%d/%s#%s", rnd, url[strings.Index(url, "/api/")+5:], secret)))
+
+	return fmt.Sprintf("%d%x", rnd, SHA512.Sum(nil))
+}
+
+func GetRequest(url string) ([]byte, error) {
+	public := os.Getenv("CF_PUBLIC_KEY")
+	secret := os.Getenv("CF_SECRET_KEY")
+	if public != "" && secret != "" {
+		url += fmt.Sprintf("&apiKey=%s&time=%d", public, time.Now().Unix())
+		url += fmt.Sprintf("&apiSig=%s", GenerateAPISig(SortedParams(url), secret))
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	response := struct {
+		Status  string          `json:"status"`
+		Result  json.RawMessage `json:"result"`
+		Comment string          `json:"comment"`
+	}{}
+	if err = json.Unmarshal(body, &response); err != nil {
+		return nil, err
+	}
+
+	if response.Status != "OK" {
+		return nil, fmt.Errorf(`codeforces API returned status %s with error message "%s"`, response.Status, response.Comment)
+	}
+
+	return response.Result, nil
+}
+
+func (blogEntry *BlogEntry) GetComments() ([]Comment, error) {
+	resp, err := GetRequest(fmt.Sprintf("https://codeforces.com/api/blogEntry.comments?blogEntryId=%d", blogEntry.ID))
+	if err != nil {
+		return nil, err
+	}
+
+	comments := []Comment{}
+	if err = json.Unmarshal(resp, &comments); err != nil {
+		return nil, err
+	}
+
+	return comments, nil
+}
+
+func (blogEntry *BlogEntry) GetView() (BlogEntry, error) {
+	resp, err := GetRequest(fmt.Sprintf("https://codeforces.com/api/blogEntry.view?blogEntryId=%d", blogEntry.ID))
+	if err != nil {
+		return BlogEntry{}, err
+	}
+
+	blogEntryResult := BlogEntry{}
+	if err = json.Unmarshal(resp, &blogEntryResult); err != nil {
+		return BlogEntry{}, err
+	}
+
+	return blogEntryResult, nil
+}
+
+func (contest *Contest) GetHacks() ([]Hack, error) {
+	resp, err := GetRequest(fmt.Sprintf("https://codeforces.com/api/contest.hacks?contestId=%d", contest.ID))
+	if err != nil {
+		return nil, err
+	}
+
+	hacks := []Hack{}
+	if err = json.Unmarshal(resp, &hacks); err != nil {
+		return nil, err
+	}
+
+	return hacks, nil
+}
+
+func GetContestList(gym bool) ([]Contest, error) {
+	resp, err := GetRequest(fmt.Sprintf("https://codeforces.com/api/contest.list?gym=%t", gym))
+	if err != nil {
+		return nil, err
+	}
+
+	contests := []Contest{}
+	if err = json.Unmarshal(resp, &contests); err != nil {
+		return nil, err
+	}
+
+	return contests, nil
+}
+
+func (contest *Contest) GetRatingChanges() ([]RatingChange, error) {
+	resp, err := GetRequest(fmt.Sprintf("https://codeforces.com/api/contest.ratingChanges?contestId=%d", contest.ID))
+	if err != nil {
+		return nil, err
+	}
+
+	ratingChanges := []RatingChange{}
+	if err = json.Unmarshal(resp, &ratingChanges); err != nil {
+		return nil, err
+	}
+
+	return ratingChanges, nil
+}
+
+func (contest *Contest) GetStandings(from, count int, handles []string, room int, showUnofficial bool) (Standings, error) {
+	url := fmt.Sprintf("https://codeforces.com/api/contest.standings?contestId=%d&showUnofficial=%t", contest.ID, showUnofficial)
+	if from > 1 {
+		url += fmt.Sprintf("&from=%d", from)
+	}
+	if count > 0 {
+		url += fmt.Sprintf("&count=%d", count)
+	}
+	if len(handles) > 0 {
+		url += fmt.Sprintf("&handles=%s", strings.Join(handles, ";"))
+	}
+	if room > 0 {
+		url += fmt.Sprintf("&room=%d", room)
+	}
+
+	resp, err := GetRequest(url)
+	if err != nil {
+		return Standings{}, err
+	}
+
+	standings := Standings{}
+	if err = json.Unmarshal(resp, &standings); err != nil {
+		return Standings{}, err
+	}
+
+	return standings, nil
+}
+
+func (contest *Contest) GetStatus(from, count int, handle string) ([]Submission, error) {
+	url := fmt.Sprintf("https://codeforces.com/api/contest.status?contestId=%d", contest.ID)
+	if from > 1 {
+		url += fmt.Sprintf("&from=%d", from)
+	}
+	if count > 0 {
+		url += fmt.Sprintf("&count=%d", count)
+	}
+	if handle != "" {
+		url += fmt.Sprintf("&handle=%s", handle)
+	}
+
+	resp, err := GetRequest(url)
+	if err != nil {
+		return nil, err
+	}
+
+	submissions := []Submission{}
+	if err = json.Unmarshal(resp, &submissions); err != nil {
+		return nil, err
+	}
+
+	return submissions, nil
+}
+
+func GetProblems(tags []string, problemsetName string) ([]Problem, error) {
+	url := "https://codeforces.com/api/problemset.problems?"
+	if len(tags) > 0 {
+		url += fmt.Sprintf("tags=%s", strings.Join(tags, ";"))
+	}
+	if problemsetName != "" {
+		if url[len(url)-1] != '?' {
+			url += "&"
+		}
+		url += fmt.Sprintf("problemsetName=%s", problemsetName)
+	}
+
+	resp, err := GetRequest(url)
+	if err != nil {
+		return nil, err
+	}
+
+	problems := []Problem{}
+	if err = json.Unmarshal(resp, &problems); err != nil {
+		return nil, err
+	}
+
+	return problems, nil
+}
+
+func GetRecentSubmissions(count int, problemsetName string) ([]Submission, error) {
+	url := "https://codeforces.com/api/recentActions?"
+	if count > 0 {
+		url += fmt.Sprintf("count=%d", count)
+	}
+	if problemsetName != "" {
+		if url[len(url)-1] != '?' {
+			url += "&"
+		}
+		url += fmt.Sprintf("problemsetName=%s", problemsetName)
+	}
+
+	resp, err := GetRequest(url)
+	if err != nil {
+		return nil, err
+	}
+
+	submissions := []Submission{}
+	if err = json.Unmarshal(resp, &submissions); err != nil {
+		return nil, err
+	}
+
+	return submissions, nil
+}
+
+func GetRecentActions(maxCount int) ([]RecentAction, error) {
+	url := "https://codeforces.com/api/recentActions?"
+	if maxCount > 0 {
+		url += fmt.Sprintf("maxCount=%d", maxCount)
+	}
+
+	resp, err := GetRequest(url)
+	if err != nil {
+		return nil, err
+	}
+
+	actions := []RecentAction{}
+	if err = json.Unmarshal(resp, &actions); err != nil {
+		return nil, err
+	}
+
+	return actions, nil
+}
+
+func (user *User) GetBlogEntries() ([]BlogEntry, error) {
+	resp, err := GetRequest(fmt.Sprintf("https://codeforces.com/api/user.blogEntries?handle=%s", user.Handle))
+	if err != nil {
+		return nil, err
+	}
+
+	blogEntries := []BlogEntry{}
+	if err = json.Unmarshal(resp, &blogEntries); err != nil {
+		return nil, err
+	}
+
+	return blogEntries, nil
+}
+
+func (user *User) GetFriends(onlyOnline bool) ([]User, error) {
+	resp, err := GetRequest(fmt.Sprintf("https://codeforces.com/api/user.friends?onlyOnline=%t&handle=%s", onlyOnline, user.Handle))
+	if err != nil {
+		return nil, err
+	}
+
+	friends := []User{}
+	if err = json.Unmarshal(resp, &friends); err != nil {
+		return nil, err
+	}
+
+	return friends, nil
+}
+
+func (user *User) GetInfo() (User, error) {
+	resp, err := GetRequest(fmt.Sprintf("https://codeforces.com/api/user.info?handles=%s", user.Handle))
+	if err != nil {
+		return User{}, err
+	}
+
+	users := []User{}
+	if err = json.Unmarshal(resp, &users); err != nil {
+		return User{}, err
+	}
+
+	return users[0], nil
+}
+
+func GetUsersInfo(handles []string) ([]User, error) {
+	resp, err := GetRequest(fmt.Sprintf("https://codeforces.com/api/user.info?handles=%s", strings.Join(handles, ";")))
+	if err != nil {
+		return nil, err
+	}
+
+	users := []User{}
+	if err = json.Unmarshal(resp, &users); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func GetRatedList(activeOnly, includeRetired bool, contestId int) ([]User, error) {
+	url := "https://codeforces.com/api/user.ratedList?"
+	if activeOnly {
+		url += "activeOnly=true"
+	}
+	if includeRetired {
+		if url[len(url)-1] != '?' {
+			url += "&"
+		}
+		url += "includeRetired=true"
+	}
+	if contestId > 0 {
+		if url[len(url)-1] != '?' {
+			url += "&"
+		}
+		url += fmt.Sprintf("contestId=%d", contestId)
+	}
+
+	resp, err := GetRequest(url)
+	if err != nil {
+		return nil, err
+	}
+
+	users := []User{}
+	if err = json.Unmarshal(resp, &users); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (user *User) GetRating() ([]RatingChange, error) {
+	resp, err := GetRequest(fmt.Sprintf("https://codeforces.com/api/user.rating?handle=%s", user.Handle))
+	if err != nil {
+		return nil, err
+	}
+
+	ratingChanges := []RatingChange{}
+	if err = json.Unmarshal(resp, &ratingChanges); err != nil {
+		return nil, err
+	}
+
+	return ratingChanges, nil
+}
